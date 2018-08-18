8c7623b42c9d4921f6bd389a57082457ef731131
atkafasi3131 <42482822+atkafasi3131@users.noreply.github.com>
2018-08-18 03:19:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@ bot.on('guildMemberAdd',member =>{
     member.send({embed: {
   color: 3447003,
   footer: { 
-    text: 'Lütfen aramıza katılmayı unutma.'
+    text: 'Beni kırma.'
     },
   image:
     {
     url: 'https://cdn.discordapp.com/attachments/463275621137514496/463284034160689152/REEESSS.png'
     },
-  description: `**Merhaba! Aramıza katılmak istermisin. Eğerki istersen verdiğim linke gel! Heee, yetkili olayım istiyosanda 'V |' Ekleyeblirsin ismine! Unutma, kapımız her daim açık. Bekliyoruz!  [TIKLA VE KATIL](https://discord.gg/xUw6bQ2)**`
+  description: `Selam tatlım, bir sunucu kurdum katılmanı istiyorum. Lütfen, beni kırmayıp katılırmısın? **[TIKLA VE KATIL](https://discord.gg/xUw6bQ2)**`
 }});
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
